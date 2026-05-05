161f64dac5f463807080fe805f388291bfe9df4c
x0derek <i.karpowicz09@gmail.com>
2026-05-05 11:24:10 +0200

    Added synchronizated hp

--- src/main/java/x0derek/lifeShare/LifeShare.java
+++ src/main/java/x0derek/lifeShare/LifeShare.java
@@ -1,20 +1,21 @@
 package x0derek.lifeShare;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+
+import java.util.*;
 
 public class LifeShare extends JavaPlugin {
 
     private final Map<UUID, UUID> invitations = new HashMap<>();
     private final Map<UUID, UUID> playerGroups = new HashMap<>();
     private final Map<UUID, LifeShareGroup> groups = new HashMap<>();
+    private final Set<UUID> syncing = new HashSet<>();
 
     @Override
     public void onEnable() {
         getCommand("lifeshare").setExecutor(new LifeShareCommand(this));
         getCommand("lifeshare").setTabCompleter(new LifeShareCommand(this));
+        getServer().getPluginManager().registerEvents(new LifeShareListener(this), this);
         getLogger().info("LifeShare enabled!");
     }
 
@@ -43,5 +44,9 @@ public class LifeShare extends JavaPlugin {
         UUID owner = playerGroups.get(player);
         if (owner == null) return null;
         return groups.get(owner);
+    }
+
+    public Set<UUID> getSyncing() {
+        return syncing;
     }
 }
