7fe471769b02bea3f5c3f829bb2a8f283f7e438c
x0derek <i.karpowicz09@gmail.com>
2026-05-05 11:24:10 +0200

    Added synchronizated hp

--- src/main/java/x0derek/lifeShare/LifeShareListener.java
+++ src/main/java/x0derek/lifeShare/LifeShareListener.java
@@ -1,4 +1,43 @@
 package x0derek.lifeShare;
 
-public class LifeShareListener {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+
+public class LifeShareListener implements org.bukkit.event.Listener {
+
+    private final LifeShare plugin;
+
+    public LifeShareListener(LifeShare plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+        if (!plugin.isInGroup(player.getUniqueId())) {
+            return;
+        }
+        var getDamage = event.getDamage();
+        LifeShareGroup group = plugin.getGroup(player.getUniqueId());
+        group.getMembers().forEach(member -> {
+            Player playerToDamage = Bukkit.getPlayer(member);
+            if (player.getUniqueId().equals(member)) {
+                return;
+            }
+            if (plugin.getSyncing().contains(player.getUniqueId())) {
+                return;
+            }
+            if (playerToDamage == null) {
+                return;
+            }
+            plugin.getSyncing().add(playerToDamage.getUniqueId());
+            playerToDamage.damage(event.getDamage());
+            plugin.getSyncing().remove(playerToDamage.getUniqueId());
+        });
+    }
 }
